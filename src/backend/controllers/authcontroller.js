487688fcf9e28fe0e740fea54ad93d07d157fd9f
satirolanister <j.garzonsoto@hotmail.com>
2020-09-12 19:57:47 -0500

    Aunthentication and register users

--- src/backend/controllers/authcontroller.js
+++ src/backend/controllers/authcontroller.js
@@ -1,25 +1,43 @@
 const auth = {};
 const bcrypt = require('bcryptjs');
+const moment = require('moment');
+const jwt = require('jwt-simple');
 const {user, role} = require('../database');
 
 
-auth.register = (req, res) => {
-    
+auth.register =  async (req, res) => {
+
+    req.body.Password = bcrypt.hashSync(req.body.Password, 10);
+    const User = await user.create(req.body);
+    res.json({
+        message: 'User Created'
+    });
+
 }
 
 auth.login =  async (req, res) => {
-    const User = await user.findOne({where: {Username:req.body.username}})
+    const User = await user.findOne({where: {Username:req.body.Username}})
     
     if(User){
-        const Pass = bcrypt.compareSync(req.body.password, user.password);
+        const Pass = bcrypt.compareSync(req.body.Password, User.Password);
         if(Pass){
-            res.json({success: 'TOKEN'});
+            res.json({success: createToken(User)});
         }else{
             res.json({error: 'Error en usuario y/o contraseña'});
         }
     }else{
         res.json({error: 'Error en usuario y/o contraseña'});
     }
+}
+
+const createToken = (user) => {
+    const payload = {
+        userId: user.Id_Cel,
+        roleId: user.Role_Id,
+        createdAt: moment().unix(),
+        expiredAt: moment().add(15, 'minutes').unix()
+    }
+    return jwt.encode(payload, 'Secret letter');
 }
 
 
--- src/backend/routes/auth.js
+++ src/backend/routes/auth.js
@@ -1,8 +1,8 @@
 const {Router} = require('express');
 const router = Router();
-const { login,register } = require('../controllers/authcontroller')
+const { login,register } = require('../controllers/authcontroller');
 
-router.post('/register', register);
+router.post('/register',register);
 
 router.post('/login', login);
 
