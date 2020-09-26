7e8297b5608d5b8f13c52b12f84504f931357703
satirolanister <j.garzonsoto@hotmail.com>
2020-09-25 21:29:37 -0500

    modificación userg

--- src/backend/routes/user.js
+++ src/backend/routes/user.js
@@ -1,9 +1,9 @@
 const {Router} = require('express');
 const router = Router();
 const { register } = require('../controllers/usercontroller');
-const authToken = require('../middlewares/Authtoken');
 
-router.post('/register',authToken.validaToken,register);
+
+router.post('/register',register);
 
 
 module.exports = router;
